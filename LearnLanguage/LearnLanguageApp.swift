c06873d2c4b4aa0bc2946fefe4b3e4c68b01f676
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-09-27 11:25:08 +0300

    Initial Commit

---
+++ LearnLanguage/LearnLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLanguageApp.swift
+//  LearnLanguage
+//
+//  Created by ceksi on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
